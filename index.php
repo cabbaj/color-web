bdef948bb143482ce53388241f189a61c49d8fd5
cabbaj <ppvadon@gmail.com>
2025-05-10 15:56:49 +0700

    connect the db

--- index.php
+++ index.php
@@ -1,9 +1,33 @@
 <?php
-$temp = 10;
+require("connect.php");
 ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?php if ($temp == 30): ?>
-    <p>hot</p>
-<?php elseif ($temp == 10): ?>
-    <p>cold</p>
-<?php endif; ?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <?php
+        $sql = "SELECT * FROM color";
+        $result = mysqli_query($conn, $sql);
+        // MYSQLI_ASSOC is define type of array
+        $colors = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        ?>
+        <h3>Found the colors <?php echo count($colors) ?> list</h3>
+        <?php foreach ($colors as $color): ?>
+            <div>
+                <?php echo "<h4>{$color["title"]}</h4>" ?>
+                <?php echo "<p>{$color["code"]}</p>" ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</body>
+
+</html>
+
+<?php mysqli_close($conn); ?>
